5014fdfb61910616a8641cae4579fa0bf94204b7
GROUPINFRA\vinh.tran <vinh.tran@cgi.com>
2021-01-19 09:30:01 -0700

    update auth backend

--- D_and_A_project/DjangoAPI/EmployeeAPP/urls.py
+++ D_and_A_project/DjangoAPI/EmployeeAPP/urls.py
@@ -4,14 +4,22 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import url
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('department/', views.departmentApi),
-    path('department/<int:id>', views.departmentApi),
+    # path('department/', views.departmentApi),
+    # path('department/<int:id>', views.departmentApi),
+    path('department/', views.departmentView.as_view()),
+    path('department/<int:id>', views.departmentView.as_view()),
     path('employee/', views.employeeApi),
     path('employee/<int:id>', views.employeeApi),
 
     #url(r'^SaveFile$', views.SaveFile)   
     path('SaveFile/', views.SaveFile),
-] 
+    path('auth/', obtain_auth_token),
+]
+    
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
+
+# user vinhtran pass Welcome12345
--- D_and_A_project/DjangoAPI/EmployeeAPP/views.py
+++ D_and_A_project/DjangoAPI/EmployeeAPP/views.py
@@ -1,20 +1,78 @@
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
+#from django.views.decorators.csrf import csrf_exempt
 from rest_framework.fields import NullBooleanField
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
+from rest_framework.views import APIView
 
 from EmployeeAPP.models import Departments, Employees
 from EmployeeAPP.serializers import DepartmentSerializer, EmployeeSerializer
 
 from django.core.files.storage import default_storage
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 # Create your views here.
 
-@csrf_exempt
-def departmentApi(request, id=0):
-    #check for GET method
-    if request.method == "GET": 
+# def departmentApi(request, id=0):
+    #     #check for GET method
+    #     if request.method == "GET": 
+    #         if id >  0 :
+    #             departments = Departments.objects.filter(DepartmentId = id)
+    #         else:
+    #             departments = Departments.objects.all()
+    #         department_serializer   = DepartmentSerializer(departments, many=True)
+    #         return JsonResponse(department_serializer.data, safe=False)
+
+    #     # POST method
+    #     elif request.method == 'POST':
+    #         # get data from request and parse as Json format
+    #         department_data         = JSONParser().parse(request)
+    #         department_serializer   = DepartmentSerializer(data=department_data)
+
+    #         #check if data is valid
+    #         if department_serializer.is_valid():
+    #             department_serializer.save()
+    #             return JsonResponse("Add successfuly ", safe=False)
+    #         return JsonResponse("Failed to add", safe=False)
+
+    #     # PUT method ( update an existing records )
+    #     elif request.method == "PUT": 
+    #         department_data         = JSONParser().parse(request)
+    #         # locate the data in database using "id"
+    #         department              = Departments.objects.get(DepartmentId = department_data['DepartmentId'])
+    #         department_serializer   = DepartmentSerializer(department, data=department_data)
+
+    #         #check if data is valid
+    #         if department_serializer.is_valid():
+    #             department_serializer.save()
+    #             return JsonResponse("Edited successfuly ", safe=False)
+    #         return JsonResponse("Failed to edit", safe=False)
+
+    #     elif request.method == "DELETE":
+
+    #         try:
+    #             if id > 0 :
+    #                 department  = Departments.objects.get(DepartmentId = id)
+    #             elif id == 0 :
+    #                 department_data = JSONParser().parse(request)
+    #                 department  = Departments.objects.get(DepartmentId = department_data['DepartmentId'])
+    #             else : 
+    #                 return JsonResponse("Unknow error at Delete", safe=False)
+
+    #             department.delete()
+
+    #             return JsonResponse("Delete Successful", safe=False)
+    #         except:
+    #             return JsonResponse("DElete Error", safe=False)
+class departmentView(APIView ):
+
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    
+    def get( self, request, id =0, format = None):
+        #check for GET method   
         if id >  0 :
             departments = Departments.objects.filter(DepartmentId = id)
         else:
@@ -22,9 +80,8 @@ def departmentApi(request, id=0):
         department_serializer   = DepartmentSerializer(departments, many=True)
         return JsonResponse(department_serializer.data, safe=False)
 
-    # POST method
-    elif request.method == 'POST':
-        # get data from request and parse as Json format
+    def post(self, request, id=0, format = None):
+        #get data from request and parse as Json format
         department_data         = JSONParser().parse(request)
         department_serializer   = DepartmentSerializer(data=department_data)
 
@@ -34,8 +91,7 @@ def departmentApi(request, id=0):
             return JsonResponse("Add successfuly ", safe=False)
         return JsonResponse("Failed to add", safe=False)
 
-    # PUT method ( update an existing records )
-    elif request.method == "PUT": 
+    def put(self, request, id=0, format = None):
         department_data         = JSONParser().parse(request)
         # locate the data in database using "id"
         department              = Departments.objects.get(DepartmentId = department_data['DepartmentId'])
@@ -47,8 +103,7 @@ def departmentApi(request, id=0):
             return JsonResponse("Edited successfuly ", safe=False)
         return JsonResponse("Failed to edit", safe=False)
 
-    elif request.method == "DELETE":
-
+    def delete(self, request, id=0, format=None): 
         try:
             if id > 0 :
                 department  = Departments.objects.get(DepartmentId = id)
@@ -64,9 +119,12 @@ def departmentApi(request, id=0):
         except:
             return JsonResponse("DElete Error", safe=False)
 
-
-@csrf_exempt
+    
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ))
 def employeeApi(request, id=0):
+    permission_classes = (IsAuthenticated,)
+
     #check for GET method
     if request.method == "GET": 
         if id >  0 :
@@ -119,7 +177,6 @@ def employeeApi(request, id=0):
             return JsonResponse("DElete Error", safe=False)
 
 
-@csrf_exempt
 def SaveFile(request): 
     file = request.FILES['uploadedFile']
     file_name = default_storage.save(file.name, file)
